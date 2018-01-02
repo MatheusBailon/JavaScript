cacf5f60b68f6e320f039d3b48ac20283ef484f9
Matheus <31230365+MatheusBailon@users.noreply.github.com>
2018-01-01 22:37:49 -0200

    Feito a segunda parte do desafio
    O jogador pode colocar o nome e qual o valor para ganhar o jogo.

--- Desafio 3.js
+++ Desafio 3.js
@@ -9,26 +9,30 @@ GAME RULES:
 
 */
 
-/*
-DESAFIO:
-
-1. A player loose his ENTIRE score when he rolls two 6 in a row. After that, it's the next player's turn.
-(Hint: Always save the previuous dice roll in a separate variable)
-2. Add an input field to the HTML where players can set the winnin score, so that they can change the predefined
-score of 100. (Hint: you can read that value with the .value property in JavaScript. This is a good oportunity
-to use google to figure this out :)
-3. Add another dice to the game, so tht there are two dice now. The player looses his current score when one of
-them is a 1. (Hint: you will need CSS to position the second dice, so take a look at the CSS code for the first one.)
-
-
-
-*/
-
 //activePlaye=0 para o primeiro jogador e 1 para o segundo jogador
 
-var roundScore, score, activePlayer, gamePlaying, seizao, valUser;
+var roundScore, score, activePlayer, gamePlaying, seizao, valUser, play1, play2;
 
-init();
+function enter(e){
+    if (e.which === 13)
+        document.querySelector('.btn-send').click();
+}
+
+document.addEventListener('keypress',enter);
+
+document.querySelector('.btn-send').addEventListener('click',function(){
+    var form = document.querySelector('.form');
+    var gameSpace = document.querySelector('.wrapper');
+    
+    form.style.display = 'none';
+    gameSpace.style.display = 'block';
+    
+    document.removeEventListener('keypress',enter);
+    
+    init();
+    
+})
+
 
 
 /*O comando document.querySelector('#[variavel nomeada por um id]')
@@ -48,35 +52,36 @@ Este comando seleciona um elemento do html que esta identificado por algum id
 document.querySelector('.btn-roll').addEventListener('click', function(){
     if(gamePlaying){
             
-        var dice = Math.floor(Math.random() * 6) +1;
-		
-        
-        dice === 6 ? seizao += dice : seizao = 0;
-		
-        
-        var diceDOM = document.querySelector('.dice');
-        diceDOM.style.display = 'block';
-        diceDOM.src = 'dice-' + dice + '.png';
-		
-
-        if( dice !== 1 && seizao !== 12){
-            roundScore += dice;
-            document.getElementById('current-' + activePlayer).textContent = roundScore;
-
-        } else {
-			
-			//verifica se foi rodado o numero 6 em seguida
-			var zerascore = document.getElementById('score-' + activePlayer);
-			(seizao === 12) ? (zerascore.textContent = 0, score[activePlayer] = 0 ) : seizao = 0;
-
+        if (dado('dice') || dado('dado')) {
+            
+            var zerascore = document.getElementById('score-' + activePlayer);
+            seizao === 12 ? (zerascore.textContent = 0, score[activePlayer]=0) : seizao = 0;
             nextPlayer();
-
             //document.querySelector('.player-0-panel').classList.remove('active');
             //document.querySelector('.player-1-panel').classList.add('active');
         }
     }
 
 })
+
+function dado(diceName){
+    
+    var dice = Math.floor(Math.random() * 6) +1;
+
+    dice === 6 ? seizao += 6 : seizao = 0;
+
+
+    var diceDOM = document.querySelector('.'+diceName);
+    diceDOM.style.display = 'block';
+    diceDOM.src = diceName + '-' + dice + '.png';
+
+    if( dice !== 1 && seizao !== 12){
+        roundScore += dice;
+        document.getElementById('current-' + activePlayer).textContent = roundScore;
+        return false;
+    } else 
+        return true;
+}
 
 
 document.querySelector('.btn-hold').addEventListener('click', function(){
@@ -88,7 +93,7 @@ document.querySelector('.btn-hold').addEventListener('click', function(){
         document.getElementById('score-' + activePlayer).textContent = score[activePlayer];
 
 
-        if(score[activePlayer] >= 100 ){
+        if(score[activePlayer] >= valUser ){
 
         document.querySelector('#name-' + activePlayer).textContent = 'Vencedor';
         document.querySelector('.dice').style.display = 'none';
@@ -105,13 +110,14 @@ document.querySelector('.btn-hold').addEventListener('click', function(){
 
 
 
-//Funçao para trocar o jogador q ativo (Que esta rolando os dados)
+//Funçao para trocar o jogador q está ativo (Que esta rolando os dados)
 
 function nextPlayer (){
     
     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
     roundScore = 0;
-	seizao = 0;
+    seizao = 0;
+    
         
     document.querySelector('#current-0').textContent = '0';
     document.querySelector('#current-1').textContent = '0';
@@ -120,6 +126,7 @@ function nextPlayer (){
     document.querySelector('.player-1-panel').classList.toggle('active');
         
     document.querySelector('.dice').style.display = 'none';
+    document.querySelector('.dado').style.display = 'none';
 }
 
 
@@ -132,15 +139,23 @@ function init(){
     gamePlaying = true;
     seizao = 0;
     
-    valUser = document.querySelector('#name-1').textContent;
+    play1 = document.getElementById('nomear-0').value;
+    play2 = document.getElementById('nomear-1').value;
+    valUser = document.querySelector('#valorToWin').value;
+    
+    var displayName1 = document.getElementById('name-0');
+    var displayName2 = document.getElementById('name-1');
     
     document.getElementById('score-0').textContent = 0;
     document.getElementById('score-1').textContent = 0;
     document.getElementById('current-0').textContent = 0;
     document.getElementById('current-1').textContent = 0;
     
-    document.getElementById('name-0').textContent = 'Jogador 1';
-    document.getElementById('name-1').textContent = 'Jogador 2';
+    play1 === "" ? displayName1.textContent = 'Jogador 1' : displayName1.textContent = play1;
+    play2 === "" ? displayName2.textContent = 'Jogador 2' : displayName2.textContent = play2;
+    
+    if (valUser === "")
+        valUser = 100;
     
     document.querySelector('.player-0-panel').classList.remove('winner');
     document.querySelector('.player-1-panel').classList.remove('winner');
@@ -148,7 +163,9 @@ function init(){
     document.querySelector('.player-0-panel').classList.remove('active');
     document.querySelector('.player-0-panel').classList.add('active');
     
-    document.querySelector('.dice').style.display = 'none';    
+    document.querySelector('.dice').style.display = 'none';
+    document.querySelector('.dado').style.display = 'none';
+
 }
 
 
@@ -158,7 +175,11 @@ function init(){
 
 A player looses his ENTIRE score when he rolls two 6 in a row
 
+Added the input field in the HTML, and added fields to two players
+
 */
+
+
 
 
 
